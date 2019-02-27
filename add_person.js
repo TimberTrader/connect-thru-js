ae8831d275d17b649bbe00f94ea4dc8f426136d1
Tim Bowes <timber.trader@Tims-MacBook-Pro.local>
2019-02-26 20:35:20 -0700

    refactored to use promises .then

--- add_person.js
+++ add_person.js
@@ -8,16 +8,15 @@ let firstName = process.argv[2];
 let lastName = process.argv[3];
 let birthdate = process.argv[4];
 
-knex.select('*')
-    .from('famous_people')
+knex('famous_people')
+    .insert({first_name: `${firstName}`, last_name: `${lastName}`, birthdate: `${birthdate}`})
     .then(function (result) {
-      knex.insert({first_name: `${firstName}`, last_name: `${lastName}`, birthdate: `${birthdate}`})
-          .into('famous_people');
-      console.log(result);
-  }).catch((err) => { console.log( err); throw err })
+      return knex('famous_people').select()
+    }).then((response) => {
+      console.log(response)
+    }).catch((err) => { console.log( err); throw err })
     .finally(() => {
       knex.destroy();
   });
-
 
 

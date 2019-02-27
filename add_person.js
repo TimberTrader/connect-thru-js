00b49f7eb4616277cb0d20edef48bc637d745f02
Tim Bowes <timber.trader@Tims-MacBook-Pro.local>
2019-02-26 20:12:33 -0700

    added capture person from cli & updated requirements

--- add_person.js
+++ add_person.js
@@ -1,20 +1,17 @@
 const settings = require('./settings');
 const knex = require('knex')({
   client     : 'pg',
-  connection : {
-    host    : settings.hostname,
-    user    : settings.user,
-    password: settings.password,
-    database: settings.database,
-    port    : settings.port,
-    ssl     : settings.ssl
-  }
+  connection : require('./settings')
 });
+
+let firstName = process.argv[2];
+let lastName = process.argv[3];
+let birthdate = process.argv[4];
 
 knex.select('*')
     .from('famous_people')
     .then(function (result) {
-      knex.insert({first_name: 'Tim', last_name: 'Bowes', birthdate: '1961-06-25'})
+      knex.insert({first_name: `${firstName}`, last_name: `${lastName}`, birthdate: `${birthdate}`})
           .into('famous_people');
       console.log(result);
   }).catch((err) => { console.log( err); throw err })
